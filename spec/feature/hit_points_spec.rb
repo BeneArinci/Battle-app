223c466b2f012c72e8fc4bbdd1b2a78e215d16c4
Student <student@makerss-air-2.mynet>
2020-05-31 12:34:41 +0100

    starting with multiplayer game. Added switching route for switching turns. All tests passing

--- spec/feature/hit_points_spec.rb
+++ spec/feature/hit_points_spec.rb
@@ -5,20 +5,33 @@ feature 'Checks to see other players hit points' do
     sign_in_process
     expect(page).to have_content( "Bene: 100 HP")
   end
+  scenario 'see Player 1 hit points' do
+    sign_in_process
+    expect(page).to have_content ("Sophie: 100 HP")
+  end
 end
 
 feature 'Checks that player 1 can attack player 2' do
   scenario 'shows button to allow attack' do
     sign_in_process
     click_button('Attack')
-    expect(page).to have_content ("Sophie attacked Bene")
+    expect(page).to have_content ("Bene attacked Sophie")
   end
 end
 
-feature 'Allow player1 to attack player 2 and reduce their hp' do
-  scenario 'reduces hit points' do
+feature 'be attacked by Player 2' do
+  scenario 'allow player 2 to attack' do
     sign_in_process
-    click_button('Attack')
-    expect(page).to have_content("Bene HP = 90")
+    click_button 'Attack'
+    click_button 'OK'
+    click_button 'Attack'
+    expect(page).to have_content ("Sophie attacked Bene")
   end
 end
+# feature 'Allow player1 to attack player 2 and reduce their hp' do
+#   scenario 'reduces hit points' do
+#     sign_in_process
+#     click_button('Attack')
+#     expect(page).to have_content("Bene HP = 90")
+#   end
+# end
--- spec/feature/sign_in_process.rb
+++ spec/feature/sign_in_process.rb
@@ -1,7 +1,7 @@
 
 def sign_in_process
   visit("/")
-    fill_in 'player_1', with: "Sophie"
-    fill_in 'player_2', with: "Bene"
+    fill_in 'player_1', with: "Bene"
+    fill_in 'player_2', with: "Sophie"
     click_button("Submit")
 end
